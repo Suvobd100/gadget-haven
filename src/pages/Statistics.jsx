f36050f4ca5e32072c8b784b29894fa8eca8310f
suvobd100 <makbarbd@gmail.com>
2025-01-28 12:11:01 +0600

    added line chart with json data finished

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -23,34 +23,41 @@ const Statistics = () => {
   // ]
   // console.log(data);
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center bg-stone-200">
       {/* Statistics */}
       {/* <ResponsiveContainer width={"50%"} height={"50%"}> */}
-      <AreaChart width={500} height={400} data={data.sales_data}>
-        
-        <YAxis/>
-        <XAxis dataKey={"name"}/>
-        <CartesianGrid strokeDasharray={"5 5"}/>
-        <Tooltip/>
-        <Legend/>
 
+      <div className="grid grid-cols-1">
+        <div className="flex justify-center items-center">
+          <h2 className="text-purple-500 font-bold mb-6 underline">
+            Sample Line Chart By Fake jsonData
+          </h2>
+        </div>
 
-        <Area
-          type={"monotone"}
-          dataKey={"product1_sales"}
-          stroke="#2563eb"
-          fill="#3b82f6"
-          stackId={"1"}
-        />
+        <AreaChart width={500} height={400} data={data.sales_data}>
+          <YAxis />
+          <XAxis dataKey={"name"} />
+          <CartesianGrid strokeDasharray={"5 5"} />
+          <Tooltip />
+          <Legend />
 
-        <Area
-          type={"monotone"}
-          dataKey={"product2_sales"}
-          stroke="#7c3aed"
-          fill="#8b5cf6"
-          stackId={"1"}
-        />
-      </AreaChart>
+          <Area
+            type={"monotone"}
+            dataKey={"product1_sales"}
+            stroke="#2563eb"
+            fill="#3b82f6"
+            stackId={"1"}
+          />
+
+          <Area
+            type={"monotone"}
+            dataKey={"product2_sales"}
+            stroke="#7c3aed"
+            fill="#8b5cf6"
+            stackId={"1"}
+          />
+        </AreaChart>
+      </div>
       {/* </ResponsiveContainer> */}
     </div>
   );
@@ -58,8 +65,8 @@ const Statistics = () => {
   //   if(active && payload && payload.length){
   //     return(
   //       <div className="p-4 bg-slate-600 flex flex-col gap-4 rounded-md">
-  //         <p className="text-medium text-lg"> 
-  //           <P className="text-sm text-blue-400"> 
+  //         <p className="text-medium text-lg">
+  //           <P className="text-sm text-blue-400">
   //           product1_sales
   //           </P>
   //         </p>

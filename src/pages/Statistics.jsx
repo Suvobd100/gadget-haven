0374659acfdf137695a86feb9c39b28ea652ffa5
suvobd100 <makbarbd@gmail.com>
2025-01-28 11:20:50 +0600

    added area chart with data

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,27 +1,73 @@
-import { useLoaderData } from "react-router-dom"
-import { Area, AreaChart, ResponsiveContainer } from "recharts";
-
+import { useLoaderData } from "react-router-dom";
+import {
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const Statistics = () => {
-  const data =useLoaderData();
-//   [
-//     {
-//         "month": "January",
-//         "product1_sales": 1200,
-//         "product2_sales": 800
-//     },
- 
-// ]
-  console.log(data);
+  const data = useLoaderData();
+  const { month, product1_sales } = data.sales_data;
+  //   [
+  //     {
+  //         "month": "January",
+  //         "product1_sales": 1200,
+  //         "product2_sales": 800
+  //     },
+
+  // ]
+  // console.log(data);
   return (
-    <div>Statistics
-     <AreaChart width={500} height={400} data={data.sales_data}>
+    <div className="flex justify-center items-center">
+      {/* Statistics */}
+      {/* <ResponsiveContainer width={"50%"} height={"50%"}> */}
+      <AreaChart width={500} height={400} data={data.sales_data}>
+        
+        <YAxis/>
+        <XAxis dataKey={"name"}/>
+        <CartesianGrid strokeDasharray={"5 5"}/>
+        <Tooltip/>
+        <Legend/>
 
-     </AreaChart>
 
+        <Area
+          type={"monotone"}
+          dataKey={"product1_sales"}
+          stroke="#2563eb"
+          fill="#3b82f6"
+          stackId={"1"}
+        />
 
+        <Area
+          type={"monotone"}
+          dataKey={"product2_sales"}
+          stroke="#7c3aed"
+          fill="#8b5cf6"
+          stackId={"1"}
+        />
+      </AreaChart>
+      {/* </ResponsiveContainer> */}
     </div>
-  )
-}
+  );
+  // const CustomTooltip =({active, payload,label})=>{
+  //   if(active && payload && payload.length){
+  //     return(
+  //       <div className="p-4 bg-slate-600 flex flex-col gap-4 rounded-md">
+  //         <p className="text-medium text-lg"> 
+  //           <P className="text-sm text-blue-400"> 
+  //           product1_sales
+  //           </P>
+  //         </p>
 
-export default Statistics
+  //       </div>
+  //     )
+  //   }
+  // }
+};
+
+export default Statistics;
